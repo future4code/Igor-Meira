86aba3e0d12b0695b718d1fa0e8b3c50f7012eef
Igor Meira do Nascimento Silva <igormnsilva@gmail.com>
2021-02-23 20:38:09 -0300

    exercícios da aula 11 completos

---
+++ semana3/operadores/js-vanilla-template/src/index.js
@@ -0,0 +1,88 @@
+//Exercícios de interpretação de código
+
+/* 1.
+
+a. false
+b. false
+c. true
+e. boolean */
+
+
+/*2. 
+
+a. undefined
+b. null
+c. 11
+d. 3
+e. [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+f. 9 */
+
+
+// Exercícios de escrita de código
+
+// 1. 
+
+//idade = prompt('Qual a sua idade?') = 28
+//idadeAmigo = prompt('Qual a idade do seu melhor amigo?') = 27
+
+//resultado = idade > idadeAmigo
+//console.log("A sua idade é maior que a do seu melhor amigo? ", resultado) = true
+
+//console.log(idade - idadeAmigo) = 1
+
+
+//2. 
+
+//const numeroPar = prompt("Escreva um número par") 
+//const resultado = Number(numeroPar) % 2
+
+//console.log("O resto da divisão é igual a ", resultado) = 0 
+
+//Todos os resultados dão 0 pois não temos restos em divisão de números pares.
+
+//Se o usuário escrever um número ímpar, todos os restos serão 1.
+
+
+//3.
+
+
+/*let array = []
+console.log(array)
+
+let entrada1 = prompt("Digite 1 de 3 atividades dárias")
+let entrada2 = prompt("Digite 2 de 3 atividades dárias")
+let entrada3 = prompt("Digite 3 de 3 atividades dárias")
+
+array.push(entrada1)
+array.push(entrada2)
+array.push(entrada3)
+console.log(array)
+
+let pergunta = prompt('Qual tarefa você já realizou?')
+const resultado = Number(pergunta)
+console.log(resultado)
+
+array.slice(0, 1)
+console.log(array) */
+
+//4.
+
+/*let nomeDoUsuario = prompt("Escreva seu nome")
+let emailDoUsuario = prompt("E seu email")
+
+console.log("O email ", emailDoUsuario, " foi cadastrado com sucesso. Seja bem-vinda(o),",nomeDoUsuario,"!")
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
