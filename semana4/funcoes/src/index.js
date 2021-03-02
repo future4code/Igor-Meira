ca5adc9ccdd45ceb21d4fa9ab22b6f982574559e
Igor Meira do Nascimento Silva <igormnsilva@gmail.com>
2021-03-01 23:00:41 -0300

    exercício concluído

--- semana4/funcoes/src/index.js
+++ semana4/funcoes/src/index.js
@@ -33,28 +33,124 @@ multiplicado por 2 tiver o resto 0. Alteraria para arrayInicio.
 //EXERCÍCIO 4 
 
 //a.
-
 /*
-let quemSouEu = function(lista) {
-    for(item of lista) {
-        console.log(item)
-    }
-    return lista
-}
-
-const resposta = quemSouEu (["Eu Sou Igor", "Tenho 27 anos", "moro em São Paulo", "E sou estudante"])
-
-*/
-
-//b.
-
 let nome = "Igor"
 let idade = 27
 let cidade = "São Paulo"
-let souEstudante = true
+let souEstudante = "sou estudante"
 
-let quemSouEu = function (nome, idade, cidade, souEstudante ) {
-        if(souEstudante === true)
-        console.log('Meu nome é ' + nome + ', tenho ' + idade + ' anos, moro em ' + cidade + ' e sou estudante')    
+function quemSouEu(){
+        console.log('Meu nome é ' + nome + ', tenho ' + idade + ' anos, moro em ' + cidade + ' e ' + souEstudante)
+
     } 
     
+quemSouEu()
+*/
+
+
+//b.
+/*
+function informacao(nome, idade, cidade, souEstudante){
+    if(souEstudante === true){
+        souEstudante = "sou estudante"
+    } else{
+        souEstudante = "não sou estudante"
+    }
+    return 'Meu nome é ' + nome + ', tenho ' + idade + ' anos, moro em ' + cidade + ' e ' + souEstudante
+};
+
+console.log(informacao('Igor', 27, 'São Paulo', true)) 
+*/
+
+
+//EXERCÍCIO 5
+
+//.a
+/*
+function adicao(num1, num2){
+    const adicao = num1 +num2
+    return adicao
+}
+
+console.log('Soma é igual a: ', adicao(10, 12));
+
+
+//b.
+
+function comparador(num1, num2){
+    let maiorQue = num1 > num2
+    return maiorQue
+}
+
+console.log('O primeiro número é maior: ', comparador(15, 13))
+
+//c.
+
+function replicaMensagem(mensagem){
+    for(let i = 0; i < 10; i++){
+        console.log(mensagem)
+    }
+}
+
+replicaMensagem('Tomara que esteja repetindo a mensagem');
+*/
+
+
+//EXERCÍCIO 6
+
+//const array = [10, 23, 45, 78, 90, 52, 35, 67, 84, 22]
+
+//a.
+/*
+function calculoNumeros(arrayNumeros){
+    return arrayNumeros.length
+}
+
+console.log('Total de numeros: ', calculoNumeros(array))
+*/
+
+
+//b.
+/*
+function numeroPar(numero){
+    let par;
+    if(numero % 2 === 0){
+        par = true
+    } else {
+        par = false  
+    }
+    return par
+}
+
+console.log('Número par: ', numeroPar(18))
+*/
+
+//c.
+/*
+function quantidadeNumerosPares(arrayDeNumeros){
+    let numerosPares = []
+    for(const numero of arrayDeNumeros){
+        if(numero % 2 === 0){
+            numerosPares.push(numero)
+        }
+    }
+    return numerosPares.length
+}
+
+console.log("Numeros Pares: ", quantidadeNumerosPares(array))
+*/
+
+//d.
+/*
+function quantidadeNumerosParesOutra(arrayDeNumeros){
+    let numerosPares = [];
+    for(const numero of arrayDeNumeros){
+        if(quantidadeNumerosParesOutra(numero)){
+            numerosPares.push(numero)
+        }
+    }
+    return numerosPares.length
+}
+
+console.log("Numeros Pares: ", quantidadeNumerosParesOutra(array)) 
+*/
