2e735f6ad3b3d291fbad04d5a07aca1178a65adb
Igor Meira do Nascimento Silva <igormnsilva@gmail.com>
2021-04-09 15:32:18 -0300

    desenvolvendo

---
+++ projeto-astromatch/astromatch/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffects } from 'react'
+import styles from 'styled-components'
+import axios from 'axios'
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
