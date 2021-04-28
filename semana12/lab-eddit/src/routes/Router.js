a38e54e189f51648a657c07855cf5a722a70dfea
Igor Meira do Nascimento Silva <igormnsilva@gmail.com>
2021-04-28 00:02:49 -0300

    app criado, rotas e paginas criadas

---
+++ semana12/lab-eddit/src/routes/Router.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import LoginPage from '../pages/LoginPage/LoginPage'
+import SignUpPage from '../pages/SignUpPage/SignUpPage'
+import FeedPage from '../pages/FeedPage/FeedPage'
+import PostPage from '../pages/PostPage/PostPage'
+
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path='/login'>
+                    <LoginPage/>
+                </Route>
+
+                <Route exact path='cadastro'>
+                    <SignUpPage/>
+                </Route>
+
+                <Route exact path='/'>
+                    <FeedPage/>
+                </Route>
+
+                <Route exact path='/post/:id'>
+                    <PostPage/>
+                </Route>
+
+                <Route>
+                    <div>Erro!</div>
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Router
