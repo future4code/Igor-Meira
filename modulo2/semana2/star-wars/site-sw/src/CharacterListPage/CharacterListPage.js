bae374df0a26cdb0a3f1dc50f8133b663343662e
Igor Meira do Nascimento Silva <igormnsilva@gmail.com>
2022-02-08 18:40:45 -0300

    App criado

---
+++ modulo2/semana2/star-wars/site-sw/src/CharacterListPage/CharacterListPage.js
@@ -0,0 +1,4 @@
+
+export const CharacterListPage = () => {
+    return <h1>Lista de Personagens</h1>
+}
---
+++ modulo2/semana2/star-wars/site-sw/src/router.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+export const Router = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/">
+
+                </Route>
+                <Route path="/characterlist">
+
+                </Route>
+                <Route path="/details">
+
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
